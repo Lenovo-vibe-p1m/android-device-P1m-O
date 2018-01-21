aeacab5d84801a312877aca6b6f4dbc1bc76a628
DhirajSurvase <dms25121997@gmail.com>
2018-01-21 05:51:11 +0000

    build twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,20 +141,23 @@ TARGET_RECOVERY_FSTAB := device/lenovo/P1m/rootdir/root/recovery.fstab
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # TWRP stuff
+RECOVERY_VARIANT := twrp
+TW_DEVICE_VERSION := P1m
 TW_THEME := portrait_hdpi
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
 TW_MAX_BRIGHTNESS := 255
-TW_EXCLUDE_SUPERSU := true
 TW_INCLUDE_FB2PNG := true
 TW_NO_CPU_TEMP := true
 TW_REBOOT_BOOTLOADER := true
 TW_REBOOT_RECOVERY := true
-TW_HAS_DOWNLOAD_MODE := true
 TW_EXCLUDE_SUPERSU := true
 TW_USE_TOOLBOX := true
+TW_INCLUDE_CRYPTO := true
+PRODUCT_COPY_FILES += device/lenovo/P1m/rootdir/root/twrp.fstab:recovery/root/etc/twrp.fstab
+PRODUCT_COPY_FILES += device/lenovo/P1m/rootdir/root/recovery.fstab:recovery/root/etc/recovery.fstab
 
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
 
